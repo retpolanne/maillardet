eeb75a2f2fe7f45ae4878d520876b6abe6614ac9
Vinicyus Macedo <7549205+vinicyusmacedo@users.noreply.github.com>
2019-01-19 18:39:20 -0200

    Removed expected content

--- pkg/tex/bibliography_test.go
+++ pkg/tex/bibliography_test.go
@@ -3,31 +3,9 @@ package tex
 var bookReference = &ReferencedContent{}
 var websiteReference = &ReferencedContent{}
 
-var expectedBookReference = `
-@book{ibge1993,
-	Address = {Rio de Janeiro},
-	Author = {IBGE},
-	Date-Added = {2013-08-21 13:56:10 +0000},
-	Date-Modified = {2013-08-21 13:56:10 +0000},
-	Edition = {3},
-	Organization = {http://biblioteca.ibge.gov.br/visualizacao/livros/liv23907.pdf},
-	Publisher = {Centro de Documenta{\c c}{\~a}o e Dissemina{\c c}{\~a}o de Informa{\c c}{\~o}es. Funda{\c c}{\~a}o Intituto Brasileiro de Geografia e Estat{\'\i}stica},
-	Title = {Normas de apresenta{\c c}{\~a}o tabular},
-	Urlaccessdate = {21 ago 2013},
-	Year = {1993}}
-`
+var expectedBookReference = ``
 
-var expectedWebsiteReference = `
-@manual{abntex2modelo-relatorio,
-	Author = {Lauro C{\'e}sar Araujo},
-	Date-Added = {2013-01-15 00:05:34 +0000},
-	Date-Modified = {2015-04-27 22:43:18 +0000},
-	Organization = {Equipe abnTeX2},
-	Title = {Modelo Can{\^o}nico de Relat{\'o}rio T{\'e}cnico e/ou Cient{\'\i}fico com abnTeX2},
-	Url = {http://www.abntex.net.br/},
-	Year = {2015},
-	Bdsk-Url-1 = {http://www.abntex.net.br/}}
-`
+var expectedWebsiteReference = ``
 
 var providedAccentuation = `áàâãéêèîíôõóûúç`
 var expectedAccentuation = `
--- pkg/tex/cover_test.go
+++ pkg/tex/cover_test.go
@@ -2,22 +2,6 @@ package tex
 
 var cover = &Cover{}
 
-var expectedCover = `
-\titulo{Título do artigo}
-\autor{Equipe \abnTeX}
-\local{Brasil}
-\data{2018, v-1.9.7}
-\orientador{João da Silva}
-\coorientador{Equipe \abnTeX}
-\instituicao{%
-  Universidade do Brasil -- UBr
-  \par
-  Faculdade de Arquitetura da Informação
-  \par
-  Programa de Pós-Graduação}
-\tipotrabalho{Tese (Doutorado)}
-\preambulo{Modelo canônico de trabalho monográfico acadêmico em conformidade com
-as normas ABNT apresentado à comunidade de usuários \LaTeX.}
-`
+var expectedCover = ``
 
-var expectedTitle = `Modelo Canônico de\\ Trabalho Acadêmico com \abnTeX`
+var expectedTitle = ``
